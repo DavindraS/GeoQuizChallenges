f534df8b7d94440173bb1b962b68686492bedded
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-23 21:53:40 -0400

    Change prevButton and nextButton to be of type ImageButton

--- src/com/dsutraban/geoquiz/QuizActivity.java
+++ src/com/dsutraban/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,8 +13,8 @@ public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mPrevButton;
-	private Button mNextButton;
+	private ImageButton mPrevButton;
+	private ImageButton mNextButton;
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[]{
@@ -86,7 +87,7 @@ public class QuizActivity extends Activity {
 		});
 		
 		/* CHALLENGE: ADD A PREVIOUS BUTTON */
-		mPrevButton = (Button)findViewById(R.id.prev_button);
+		mPrevButton = (ImageButton)findViewById(R.id.prev_button);
 		mPrevButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -97,7 +98,7 @@ public class QuizActivity extends Activity {
 			}
 		});
 		
-		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton = (ImageButton)findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
